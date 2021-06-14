4cef899983787e4d13f8e527ec58e2fd132ee598
Fernanda <sofimosol@gmail.com>
2021-06-14 16:49:02 -0500

    primer screenshot del proyecto GitMundo "FERNADA "

---
+++ contacto.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GitMundo Contacto</title>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+<br>
+<div class="container ">
+    	<div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card justify-content-center">
+                <div class="card-header bg-success text-white">
+                	María Fernanda Solis Díaz
+                </div>
+                	
+                <div class="card-body  ">
+                    <div class="container " >
+					  <h2 class="text-center">Redes Sociales</h2>
+					  <p>
+					  	Fernanda Solis
+					  	<a href="" target="_blank"><img src="img/facebook.png"></a>
+					  </p>
+					  
+
+					  <p>
+					  	Fernanda Solis
+					  <a href=""><img src="img/instagram.png"></a>
+					  </p>
+
+					  <P>
+					  961 236 29 15
+					  <a href=""><img src="img/whatsapp.png"></a>
+					  </P>
+
+					  <p>
+					  	
+					  Sofimosol@gmail.com	<a href=""><img src="img/gmail.png"></a>
+					  </p>
+					  
+					  
+
+					 <div class="text-center">
+					 	<a href="index.php" class="btn btn-success ">Regresar</a>
+					 </div> 
+					
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GitMundo</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<br>
+
+	<div class="container text-center" >
+    	<div class="row">
+        <div class="col-md-8">
+            <div class="card">
+
+               <ul class="nav nav-tabs bg-success text-white">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Indice</a>
+                  </li>
+                  
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="contacto.php">Contacto</a>
+                  </li>
+                </ul> 
+
+                <div class="tab-content">
+                        <div id="home" class="container tab-pane active"><br>
+                          <h3>INDICE</h3>
+                          <p>Esta página web, describe dos proyectos con los que se trabajara este cuatrimestre. El proyecto que se 
+                          realizara en la materia de Web integral trata de votos en línea. Los alumnos podrán dar sus votos acerca 
+                          de lo que sucede en la universidad, como la elección de proyectos universitarios, equipos de trabajo, 
+                          elección de estudiantes para ciertas actividades, entre otras cosas.  
+                          <br>
+                          <br>
+                          En  cuanto  al  proyecto  de  la  materia  dispositivos  móviles,  aún  no  está  definitivo,  se  trabajarán  con 
+                          dispositivos weareables durante la unidad.</p>
+                        </div>
+                        
+                    </div>
+
+          
+
+                      <!-- Tab panes -->
+                      
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
